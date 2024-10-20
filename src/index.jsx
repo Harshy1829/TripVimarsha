f95a83ce97ec13faa75e91ddab18dfcd96ecf57b
Harshy1829 <harshadestinek@gmail.com>
2024-10-20 14:46:51 +0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import AuthProvider from './AuthContext'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
