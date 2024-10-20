87a7968365662d1af80d38d379cf0e5451c0a6f4
Harshy1829 <harshadestinek@gmail.com>
2024-10-20 14:03:33 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SideBar from './components/SideBar'; // Adjust the path if necessary
 
+const App = () => {
+  return (
+    <Router>
+      <div className="flex">
+        <SideBar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/trips" element={<MyTrips />} />
+            <Route path="/partners" element={<TravelPartners />} />
+            <Route path="/cost-split" element={<CostSplit />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
