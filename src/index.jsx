1247103cb0244bdd46f7036d50ae8c0290a0d13d
Harshy1829 <harshadestinek@gmail.com>
2024-10-20 14:30:10 +0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SideBar from './components/SideBar'; // Adjust path if necessary
-import Dashboard from './components/Dashboard'; // Import your Dashboard component
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 
-const App = () => {
-  return (
-    <Router>
-      <div className="flex">
-        <SideBar />
-        <div className="flex-grow p-4">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            {/* Add more routes for other components here */}
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
